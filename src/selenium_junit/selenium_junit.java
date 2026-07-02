908895fc2e90c87f1d2b7455e182077d72d43c1e
AthipMix <674259028@webmail.npru.ac.th>
2026-07-02 21:13:22 +0700

    first commit

---
+++ src/selenium_junit/selenium_junit.java
@@ -0,0 +1,50 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+class SeleniumJunitTest {
+
+    private WebDriver driver;
+
+    @BeforeEach
+    void setUp() {
+        // ทำงานก่อนเริ่มทุกๆ Test Case: เปิด Browser และไปที่ Google
+        System.setProperty("webdriver.chrome.driver", "./src/chromedriver");
+        driver = new ChromeDriver();
+        driver.get("https://www.google.com");
+    }
+
+    @AfterEach
+    void tearDown() {
+        // ทำงานหลังจบทุกๆ Test Case: ปิด Browser และคืนทรัพยากรระบบทั้งหมด
+        if (driver != null) {
+            driver.quit(); 
+        }
+    }
+
+    @Test
+    void tc_101_search_npru() {
+        WebElement searchBox = driver.findElement(By.id("APjFqb"));
+        searchBox.sendKeys("NPRU");  
+        searchBox.sendKeys(Keys.ENTER);
+        
+        // แนะนำเพิ่มเติม: ควรใส่ Assertion เพื่อเช็คผลลัพธ์ เช่น เช็ค Title ของหน้าเว็บ
+        assertTrue(driver.getTitle().contains("NPRU"));
+    }
+   
+    @Test
+    @Disabled("Temporarily disabling this test case")
+    void tc_102_search_software() {
+        WebElement searchBox = driver.findElement(By.id("APjFqb"));
+        searchBox.sendKeys("Software");  
+        searchBox.sendKeys(Keys.ENTER);
+    }
+}
